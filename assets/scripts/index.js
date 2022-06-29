8064a14e3183c83be7d7c248baa08146084c7576
MageLink <mage.link.zhezard@gmail.com>
2022-06-29 05:11:13 -0400

    Listo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,28 @@
+let productQuantity = 0;
+let price = 320000;
+let colorSelected = undefined;
+
+document.getElementById("price").innerHTML = price;
+
+const multiplyProducts = (price, quantity) => {
+  return price * quantity;
+};
+
 const probando = () => {
-  console.log("Ola k ase");
+  productQuantity = parseInt(
+    document.getElementById("quantity-products").value
+  );
+  const total = multiplyProducts(price, productQuantity);
+
+  document.getElementById("total").innerHTML = total;
+  document.getElementById("quantity").innerHTML = productQuantity;
+  document.getElementById("quantity-products").value = "";
+
+  colorSelected = document
+    .getElementById("inputState")
+    .value.toLocaleLowerCase();
+
+  document.getElementById("border-color").style.backgroundColor = document
+    .getElementById(`${colorSelected}-color`)
+    .innerHTML.toLocaleLowerCase();
 };
