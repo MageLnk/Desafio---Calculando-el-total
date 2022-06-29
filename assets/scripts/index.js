66569430dfa42b84e30037e1b2be64632ac97142
MageLink <mage.link.zhezard@gmail.com>
2022-06-29 08:25:50 -0400

    Change the function name and add the format number system

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,26 +2,30 @@ let quantityOfProducts = 0;
 let price = 320000;
 let colorSelected = undefined;
 
-document.getElementById("price").innerHTML = price;
-
+// Operators
+const formatNumbers = (number) => {
+  return number.toLocaleString("en-US");
+};
 const multiplyProducts = (price, quantity) => {
   return price * quantity;
 };
 
-const probando = () => {
+document.getElementById("price").innerHTML = formatNumbers(price);
+const operateTotal = () => {
   quantityOfProducts = parseInt(
     document.getElementById("quantity-products").value
   );
   const total = multiplyProducts(price, quantityOfProducts);
 
-  document.getElementById("total").innerHTML = total;
+  document.getElementById("total").innerHTML = formatNumbers(total);
   document.getElementById("quantity").innerHTML = quantityOfProducts;
-  document.getElementById("quantity-products").value = "";
-
   colorSelected = document
     .getElementById("inputState")
     .value.toLocaleLowerCase();
-
+  //Clear
+  document.getElementById("quantity-products").value = "";
+  document.getElementById("inputState").value = "Choose...";
+  //Pass color
   document.getElementById("border-color").style.backgroundColor = document
     .getElementById(`${colorSelected}-color`)
     .innerHTML.toLocaleLowerCase();
